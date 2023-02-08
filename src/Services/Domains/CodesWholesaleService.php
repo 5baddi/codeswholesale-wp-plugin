4c45582135ae23a1dc1946c496db08b2aca95bb8
5baddi <project@baddi.info>
2023-02-08 21:35:02 +0100

    feat: init domain service

---
+++ src/Services/Domains/CodesWholesaleService.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * PHP version 7.
+ *
+ * @category PHP
+ *
+ * @author   5baddi <project@baddi.info>
+ *
+ * @link     http://baddi.info
+ */
+
+namespace BaddiServices\CodesWholesale\Services\Domains;
+
+/**
+ * Class CodesWholesaleService.
+ *
+ * @category PHP
+ *
+ * @author   5baddi <project@baddi.info>
+ *
+ * @link     http://baddi.info
+ */
+class CodesWholesaleService
+{
+    public const LIVE_ENDPOINT_URL = 'https://api.codeswholesale.com';
+    public const SANDBOX_ENDPOINT_URL = 'https://sandbox.codeswholesale.com';
+}
